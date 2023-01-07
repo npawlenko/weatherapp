375710400e14c41509e337fd2f0c7d33e5fe374e
blinkz8 <blinkz8@protonmail.com>
2023-01-07 20:21:19 +0100

    Add database

--- server/app/app.js
+++ server/app/app.js
@@ -7,8 +7,10 @@ const {app, router} = require("./server")(
     appConfig.server.port,
     appConfig.server.https
 );
+const db = require("./database/database");
 
 module.exports = {
     app: app,
     router: router,
+    database: db
 };
