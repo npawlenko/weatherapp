a590d5a4da0b19dc395df8057e142115d1299620
blinkz8 <blinkz8@protonmail.com>
2023-01-07 22:36:46 +0100

    Fix invalid module paths

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { BASE_PATH } = require("./../../constants");
+const { BASE_PATH } = require("./../constants");
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
