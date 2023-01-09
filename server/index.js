116ce494c71a6e917d2ab75679090cc9aafeac50
Norbert Pawlenko <blinkz8@protonmail.com>
2023-01-09 23:18:44 +0100

    Fix CORS error on frontend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const {app, db} = require("./app/app");
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
