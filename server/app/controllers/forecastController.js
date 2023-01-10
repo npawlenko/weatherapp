969b7fbb4500350f8663c9bb30dad84298de5b3e
Norbert Pawlenko <blinkz8@protonmail.com>
2023-01-10 01:58:28 +0100

    Implement current weather endpoint Fix Forecast foreign key column name

--- server/app/controllers/forecastController.js
+++ server/app/controllers/forecastController.js
@@ -1,3 +1,8 @@
+const axios = require("axios");
+const {Op} = require("sequelize");
+const integrations = require("./../config/integrations.json");
+const {response} = require("../helpers/requestHelper");
+
 function forecastController(db) {
     const Joi = require("joi");
     const {City, Forecast} = db;
@@ -10,7 +15,61 @@ function forecastController(db) {
             return res.send(response(validationError.message, false));
         }
 
-        res.send("Not implemented yet");
+        const currentDate = new Date();
+        const startDate = new Date(currentDate);
+        startDate.setHours(startDate.getHours());
+        clearMinutesAndSeconds(startDate);
+        const endDate = new Date(currentDate);
+        endDate.setTime(endDate.getTime() + (60*60*1000));
+        clearMinutesAndSeconds(endDate);
+
+        const city = await City.findOne({
+            where: {
+                id: cityId
+            }
+        });
+        if(!city) {
+            return res.send(response("City not found", false));
+        }
+
+        const cachedForecast = await Forecast.findOne({
+           where: {
+               cityId: cityId,
+               date: {
+                   [Op.between]: [startDate, endDate]
+               }
+           }
+        });
+        if(cachedForecast) {
+           return res.send(cachedForecast);
+        }
+
+        try {
+            const response = await axios
+                .get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${integrations.openWeatherMap.apiKey}`);
+            const data = response.data;
+
+            const newForecast = await Forecast.create({
+                main: data.weather[0].main,
+                description: data.weather[0].description,
+                date: new Date(),
+                temperature: data.main.temp,
+                temperatureFeelsLike: data.main.feels_like,
+                humidity: data.main.humidity,
+                cloudiness: data.clouds.all,
+                pressure: data.main.pressure,
+                windSpeed: data.wind.speed,
+                windDeg: data.wind.deg,
+                visibility: data.visibility,
+                rain: data?.rain?.['1h'] ?? data?.rain?.['3h'],
+                snow: data?.snow?.['1h'] ?? data?.snow?.['3h'],
+                CityId: city.id
+            });
+
+            return res.send(newForecast);
+        } catch(e) {
+            console.error(e);
+        }
     };
 
     const todayForecast = async (req, res) => {
@@ -40,6 +99,12 @@ function forecastController(db) {
         fiveDayForecast
     };
 }
+
+const clearMinutesAndSeconds = (date) => {
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+};
 
 module.exports = forecastController;
 
--- server/app/migrations/20230107192812-create-forecast.js
+++ server/app/migrations/20230107192812-create-forecast.js
@@ -9,7 +9,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      cityId: {
+      CityId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'City',
